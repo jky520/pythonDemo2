e3cfb2d9bd6e6dcebaa3525404d1391037057060
jiang <jky1988@qq.com>
2017-10-19 00:20:27 +0800

    项目的结构以及调用其他的模块

--- day4/Atm/bin/amt.py
+++ day4/Atm/bin/amt.py
@@ -10,6 +10,6 @@ sys.path.append(BASE_DIR) # 把根目录加入到环境变量中去
 # import conf, core;
 
 from conf import settings # 导入conf目录下的settings文件
-from core import main # 导入core目录下的main文件
+from core import main # 导入core目录下的main文件怎么回事
 
 main.login()
